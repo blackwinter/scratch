0aa92de39715296cefbcafd593666a3cbcb13193
Jens Wille <jens.wille@uni-koeln.de>
2008-11-04 18:18:28 +0100

    Make ll.sh accept multiple files as arguments and include file name in output.

--- ll.sh
+++ ll.sh
@@ -2,26 +2,30 @@
 
 # Find the longest line in a file
 
-[ $# -ne 1 ] && {
-  echo "Usage: $0 <file>";
+[ $# -lt 1 ] && {
+  echo "Usage: $0 <file> ...";
   exit 1;
 }
 
-[ -r "$1" ] || {
-  echo "File not found: $1";
-  exit 1;
-}
+while [ -n "$1" ]; do
+  f="$1"; shift
 
-i=0; j=0; k=0
-
-while read l; do
-  i=$((i + 1))
-  n=$(echo "$l" | wc -c)
-  
-  [ "$n" -gt "$k" ] && {
-    k="$n";
-    j="$i";
+  [ -r "$f" ] || {
+    echo "File not found: $f" >&2;
+    continue;
   }
-done < "$1"
 
-echo "$j"
+  i=0; j=0; k=0
+
+  while read l; do
+    i=$((i + 1))
+    n=$(echo "$l" | wc -c)
+  
+    [ "$n" -gt "$k" ] && {
+      k="$n";
+      j="$i";
+    }
+  done < "$f"
+
+  echo "$j $f"
+done
