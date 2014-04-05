1a98b2ef5e9fcb83bd42c2f2b0f6c2e07275a118
Jens Wille <jens.wille@gmail.com>
2014-04-05 13:28:50 +0200

    update_thunderbird_biff_images.sh: Changed paths.

--- update_thunderbird_biff_images.sh
+++ update_thunderbird_biff_images.sh
@@ -9,19 +9,19 @@ i="n?*Mail.png"  # {no,new}Mail.png
 b="$(dirname "$0")"
 
 # paths to thunderbird and firefox
-t="$HOME/.mozilla-thunderbird"
-f="$HOME/.mozilla/firefox"
+t="$HOME/.thunderbird"
+f="$HOME/.firefox"
 
 # extension ID and path to extension's image directory
 e="{aee74dd0-6dc9-11db-9fe1-0800200c9a66}"
 p="default/extensions/$e/chrome/skin/classic/images"
 
-for d in "$t" "$f"; do
+for d in "$t"; do
   q="$d/$p"  # the actual path we're acting on
 
   if [ -d "$q" ]; then
     chmod u+w "$q"/$i
-    cp "$b"/$i "$q"
+    cp -v "$b"/$i "$q"
   else
     echo "not found: $q"
   fi
