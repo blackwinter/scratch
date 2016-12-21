4703ebf7f1f12f0f1164c950d41263e6b72f620d
Jens Wille <jens.wille@gmail.com>
2016-12-21 13:51:37 +0100

    Added organize-java-imports.rb.

---
+++ organize-java-imports.rb
@@ -0,0 +1,38 @@
+#! /usr/bin/env ruby
+
+out, imp, buf, del, com = [], Hash.new { |h, k| h[k] = [] }, [], [], nil
+
+ARGF.each { |line|
+  out << line.dup
+
+  line.sub!(/(?:\A|.*\s)\*\//, '') ? com = nil  : com ? next :
+  line.sub!(/\/\*(?:\z|\s.*)/, '') ? com = true : nil
+
+  line.sub!(/\/\/\s.*/, '')
+  line.strip!
+
+  next if line.empty?
+
+  if line =~ /\Aimport\s+.*?([^.\s*]+)\s*;/
+    imp[$1] << $. - 1
+  else
+    line.scan(/\b(#{Regexp.union(*imp.keys)})\b/) {
+      del.concat(imp.delete($1).drop(1)) if imp.key?($1)
+    }
+  end
+}
+
+del.concat(imp.values.flatten).uniq!
+
+out.each_with_index { |line, num|
+  next if del.include?(num)
+
+  if line =~ /\A(?:\/\/)?import\s+/
+    buf << line
+  else
+    puts buf.sort_by { |i| i.sub(/\A\/\//, '') }, line
+    buf.clear
+  end
+}
+
+puts buf
