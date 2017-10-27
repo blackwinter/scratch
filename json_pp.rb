abb1ddf7a92b775e4ff2f55923b0b42b964f45f6
Jens Wille <jens.wille@gmail.com>
2017-10-27 10:15:22 +0200

    json_pp.rb: Added options and rescue ParserError.

--- json_pp.rb
+++ json_pp.rb
@@ -4,13 +4,36 @@ require 'json'
 
 module JSON
 
-  K = Class.new(String) { def eql?(*) false end }
-  O = Class.new(Hash) { def []=(k, v) super(K.new(k), v) end }
+  class PP_KEY < String
+
+    def eql?(*)
+      false
+    end
+
+  end
+
+  class PP_OBJECT < Hash
+
+    def []=(k, v)
+      super(PP_KEY.new(k), v)
+    end
+
+  end
 
   def self.pp(source, opt = {})
-    pretty_generate(parse(source, opt.merge(object_class: O)))
+    pretty_generate(parse(source, opt.merge(object_class: PP_OBJECT)))
+  rescue ParserError => err
+    warn "#{err.class}: #{err}"
+    source
   end
 
 end
 
-puts JSON.pp(ARGF.read) if $0 == __FILE__
+if $0 == __FILE__
+  if ARGV.include?('-h') || ARGV.include?('--help')
+    abort "Usage: #{$0} [-h|--help] [-l|--lines] [SOURCE...]"
+  end
+
+  args = ARGV.delete('--lines') || ARGV.delete('-l') ? ARGF : [ARGF.read]
+  args.each { |source| puts JSON.pp(source) }
+end
