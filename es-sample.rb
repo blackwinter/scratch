f4bba135580d468c4fe62b4482b0af1f85959df2
Jens Wille <jens.wille@gmail.com>
2017-11-02 14:50:48 +0100

    es-sample.rb: Refactored into class; added '--limit' and glob matching for '--existing'/'--missing'.

--- es-sample.rb
+++ es-sample.rb
@@ -4,145 +4,218 @@ require 'net/http'
 require 'json'
 require 'zlib'
 
-trap(:INT) { exit 130 }
+class EsSample
 
-opts = %W[[#{help = '--help'}]]
+  DEFAULT_HOST  = 'http://localhost:9200'.freeze
+  DEFAULT_COUNT = 1
+  DEFAULT_SIZE  = 1000
+  DEFAULT_LIMIT = Float::INFINITY
+  DEFAULT_QUERY = { match_all: {} }.freeze
 
-flag = lambda { |short, long|
-  opts << "[#{short_switch = "-#{short}"}|#{long_switch = "--#{long}"}]"
-  [ARGV.delete(short_switch), ARGV.delete(long_switch)].any?
-}
+  SCROLL      = '10s'.freeze
+  SCROLL_PATH = '_search/scroll'.freeze
+  GZIP_RE     = %r{\.gz(?:ip)?\z}i.freeze
+  HEADER      = { 'Content-Type' => 'application/json' }.freeze
 
-opt = lambda { |key, value, default = nil, multi = false, &block|
-  opts << "[#{switch = "-#{key}"} <#{value}>]#{'...' if multi}"
+  NET = Hash.new { |h, k|
+    h[k] = [uri = URI(k), Net::HTTP.new(uri.hostname, uri.port).tap { |http|
+      http.use_ssl = uri.scheme == 'https'
+      http.start
+    }]
+  }
 
-  block ||= lambda { |v| v }
+  def self.traverse(value, keys = [], *path)
+    case value
+      when Hash  then value.each { |k, v| traverse(v, keys, *path, k) }
+      when Array then value.each { |   v| traverse(v, keys, *path)    }
+    end
 
-  values = []
-
-  while index = ARGV.index(switch)
-    ARGV.delete_at(index)
-    values << block[ARGV.delete_at(index)]
+    path.empty? ? keys.sort!.uniq! : keys << path.join('.')
   end
 
-  values.empty? ? default : multi ? values : values.last
-}
+  def initialize(opts = {})
+    opts.each { |k, v| respond_to?(m = "#{k}=") ?
+      send(m, v) : raise(ArgumentError, "invalid argument: #{k}") }
 
-host     = opt.(:h, :HOST, 'http://localhost:9200') { |h| h.sub(/\/*\z/, '/') }
-count    = opt.(:c, :COUNT, 1) { |c| Integer(c) unless c == '-' }
-size     = opt.(:s, :SIZE, 1000) { |s| Integer(s) }
-output   = opt.(:o, :OUTPUT)
-query    = opt.(:q, :QUERY, match_all: {}) { |q| JSON.parse(q) }
-existing = opt.(:e, :EXISTING, nil, true) { |e| e.split(',') }
-missing  = opt.(:m, :MISSING,  nil, true) { |m| m.split(',') }
+    @host  ||= DEFAULT_HOST
+    @count ||= DEFAULT_COUNT
+    @size  ||= DEFAULT_SIZE
+    @limit ||= DEFAULT_LIMIT
+    @query ||= DEFAULT_QUERY
 
-if fields = opt.(:f, :FIELDS) { |f| f.split(',') } and fields.delete('-')
-  fields.concat(existing.flatten) if existing
-  fields.concat(missing.flatten)  if missing
-end
-
-random = flag[:r, :random]
-pretty = flag[:p, :pretty]
-
-if ARGV.empty? || ARGV.include?(help)
-  puts "Usage: #{$0} #{opts.join(' ')} <INDEX>..."
-  exit
-end
-
-base, type, docs, gz, header = ARGV.shift, nil, [],
-  /\.gz(?:ip)?\z/i, { 'Content-Type' => 'application/json' }
-
-def docs.<<(*); end if ARGV.empty?
-
-scroll, scroll_ids, scroll_path = '10s', [], '_search/scroll'
-
-dump, match = JSON.method("#{pretty ? :pretty : :fast}_generate"),
-  random ? { function_score: { query: query, random_score: {} } } : query
-
-net = Hash.new { |h, k|
-  h[k] = [uri = URI(k), Net::HTTP.new(uri.hostname, uri.port).tap { |http|
-    http.use_ssl = uri.scheme == 'https'
-    http.start
-  }]
-}
-
-post = lambda { |path, data|
-  uri, http = net[host]
-  uri = uri.merge(path)
-
-  res = http.post(uri.request_uri, data.to_json, header)
-  abort "ERROR: #{res.code} #{res.msg} - #{uri}" unless res.is_a?(Net::HTTPOK)
-
-  json = JSON.parse(res.body)
-  json.key?('error') ? abort(json.inspect) : json
-}
-
-each = lambda { |json, &block|
-  json['hits']['hits'].sort_by { |doc| doc['_id'] }.each(&block) }
-
-paths = lambda { |value, keys, *path|
-  case value
-    when Hash  then value.each { |k, v|
-      paths[v, keys << (_p = path + [k]).join('.'), *_p] }
-    when Array then value.each { |v| paths[v, keys, *path] }
-  end
-}
-
-fetch = lambda { |*args, &block|
-  json = post[*args]
-
-  id = json['_scroll_id']
-  scroll_ids << id if id
-
-  id unless each.(json) { |doc|
-    paths[doc['_source'], keys = []]
-
-    next if existing && !existing.any? { |list| (list - keys).empty? }
-    next if missing  && !missing.any?  { |list| (list & keys).empty? }
-
-    break [] if count && (count -= 1) < 0
-
-    type ||= doc['_type']
-    docs << doc['_id']
-    block[doc]
-  }.empty?
-}
-
-write = lambda { |index = nil, &block|
-  _block = lambda { |io| block[lambda { |doc|
-    hash = doc['_source'] and io.puts(dump[hash]) }] }
-
-  output == '-' ? _block[$stdout] : begin
-    puts name = output || "#{index || base}.jsonl"
-
-    File.open(name, index && output ? 'a' : 'w') { |f|
-      name =~ gz ? Zlib::GzipWriter.new(f).tap(&_block).close : _block[f] }
-  end
-}
-
-make_query = lambda { |hash|
-  { query: hash, _source: fields || true, size: size } }
-
-write.() { |block|
-  q = make_query[match]
-  q[:sort] = %w[_doc] unless random
-
-  id = fetch.("#{base}/_search?scroll=#{scroll}", q, &block)
-
-  while id
-    id = fetch.("#{scroll_path}?scroll=#{scroll}", scroll_id: id, &block)
+    @conditions = { existing => true, missing => false }.delete_if { |k,| !k }
   end
 
-  unless scroll_ids.empty?
-    uri, http = net[host]
+  def each(base, &block)
+    return enum_for(__method__, base) unless block
 
-    req = Net::HTTP::Delete.new(uri.merge(scroll_path).request_uri, header)
+    reset(base)
+    fetch_base(&block)
+  end
+
+  def run(base, *rest)
+    reset(base, *rest)
+
+    write { |block| fetch_base(&block) }
+    fetch_rest(*rest) if type
+  end
+
+  attr_accessor :host, :count, :size, :limit, :output, :query,
+                :existing, :missing, :fields, :random, :pretty
+
+  attr_reader :conditions, :base, :type, :docs, :scroll_ids
+
+  private
+
+  def reset(base, *rest)
+    @base, @type, @docs, @scroll_ids = base, nil, [], []
+    def docs.<<(*); end if rest.empty?
+  end
+
+  def fetch_base(&block)
+    q = !random ? make_query(query).tap { |r| r[:sort] = %w[_doc] } :
+      make_query(function_score: { query: query, random_score: {} })
+
+    id, limit = fetch("#{base}/_search?scroll=#{SCROLL}", q, &block), self.limit
+
+    while id && (limit -= size) > 0
+      id = fetch("#{SCROLL_PATH}?scroll=#{SCROLL}", scroll_id: id, &block)
+    end
+
+    clear_scroll unless scroll_ids.empty?
+  end
+
+  def fetch_rest(*args)
+    args.each { |index| write(index) { |block|
+      docs.each_slice(size) { |ids| iterate(post("#{index}/_search",
+        make_query(ids: { type: type, values: ids })), &block) } } }
+  end
+
+  def clear_scroll
+    uri, http = NET[host]
+
+    req = Net::HTTP::Delete.new(uri.merge(SCROLL_PATH).request_uri, HEADER)
     req.body = { scroll_id: scroll_ids }.to_json
 
     http.request(req)
   end
-}
 
-ARGV.each { |index| write.(index) { |block|
-  docs.each_slice(size) { |ids| each.(post["#{index}/_search",
-    make_query[ids: { type: type, values: ids }]], &block) } } } if type
+  def write(index = nil, &block)
+    write = lambda { |io| block[lambda { |doc|
+      hash = doc['_source'] and io.puts(dump(hash)) }] }
+
+    output == '-' ? write[$stdout] : begin
+      puts name = output || "#{index || base}.jsonl"
+
+      File.open(name, index && output ? 'a' : 'w') { |f|
+        name =~ GZIP_RE ? Zlib::GzipWriter.new(f).tap(&write).close : write[f]
+      }
+    end
+  end
+
+  def make_query(hash)
+    { query: hash, _source: fields || true, size: size }
+  end
+
+  def fetch(*args, &block)
+    json = post(*args)
+
+    id = json['_scroll_id']
+    scroll_ids << id if id
+
+    id unless iterate(json) { |doc|
+      unless conditions.empty?
+        keys = self.class.traverse(doc['_source'])
+
+        next if conditions.any? { |lists, condition|
+          lists.all? { |list|
+            list.any? { |glob|
+              keys.any? { |key|
+                File.fnmatch?(glob, key) } ^ condition } } }
+      end
+
+      break [] if count && (self.count -= 1) < 0
+
+      @type ||= doc['_type']
+      docs << doc['_id']
+      block[doc]
+    }.empty?
+  end
+
+  def post(path, data)
+    uri, http = NET[host]
+    uri = uri.merge(path)
+
+    res = http.post(uri.request_uri, data.to_json, HEADER)
+    raise "#{res.code} #{res.msg} - #{uri}" unless res.is_a?(Net::HTTPOK)
+
+    json = JSON.parse(res.body)
+    json.key?('error') ? raise(json.inspect) : json
+  end
+
+  def iterate(json, &block)
+    json['hits']['hits'].sort_by { |doc| doc['_id'] }.each(&block)
+  end
+
+  def dump(hash)
+    pretty ? JSON.pretty_generate(hash) : JSON.fast_generate(hash)
+  end
+
+end
+
+if $0 == __FILE__
+  trap(:INT) { exit 130 }
+
+  params, opts = %W[[#{help = '--help'}]], {}
+
+  flag = lambda { |short, long|
+    params << "[#{short_switch = "-#{short}"}|#{long_switch = "--#{long}"}]"
+    opts[long] = [ARGV.delete(short_switch), ARGV.delete(long_switch)].any?
+  }
+
+  opt = lambda { |key, name, multi = false, &block|
+    params << "[#{switch = "-#{key}"} <#{name.upcase}>]#{'...' if multi}"
+
+    block ||= lambda { |v| v }
+
+    values = []
+
+    while index = ARGV.index(switch)
+      ARGV.delete_at(index)
+      values << block[ARGV.delete_at(index)]
+    end
+
+    opts[name] = multi ? values : values.last unless values.empty?
+  }
+
+  list = lambda { |*args| opt.(*args) { |v| v.split(',') } }
+
+  opt.(:h, :host)  { |h| h.sub(/\/*\z/, '/') }
+  opt.(:c, :count) { |c| Integer(c) unless c == '-' }
+  opt.(:s, :size)  { |s| Integer(s) }
+  opt.(:l, :limit) { |l| Integer(l) unless l == '-' }
+  opt.(:o, :output)
+  opt.(:q, :query) { |q| JSON.parse(q) }
+
+  existing = list[:e, :existing, true]
+  missing  = list[:m, :missing,  true]
+
+  if fields = list[:f, :fields] and fields.delete('-')
+    fields.concat(existing.flatten) if existing
+    fields.concat(missing.flatten)  if missing
+  end
+
+  flag[:r, :random]
+  flag[:p, :pretty]
+
+  if ARGV.empty? || ARGV.include?(help)
+    puts "Usage: #{$0} #{params.join(' ')} <INDEX>..."
+    exit
+  end
+
+  begin
+    EsSample.new(opts).run(*ARGV)
+  rescue => err
+    abort "ERROR: #{err}"
+  end
+end
