5df6ae2a2a97399afec1fee1564f5f50caaf7aed
Jens Wille <jens.wille@gmail.com>
2018-02-22 16:08:07 +0100

    es-dump.sh: Optionally delete dumped index.

--- es-dump.sh
+++ es-dump.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 
-[ $# -lt 2 -o $# -gt 3 ] && echo "Usage: $0 <index> <name> [<host>]" && exit 1
+[ "$1" == "-d" ] && d=1 && shift
+[ $# -lt 2 -o $# -gt 3 ] && echo "Usage: $0 [-d] <index> <name> [<host>]" && exit 1
 
 h="${3:-http://localhost:9200}"
 o="$1"; [ "${o:$((${#o}-1))}" == "-" ] || o+="-"; o+="$2.jsonl.gz"
 
-es-sample -h "$h" -c - -o "$o" "$1" && zgrep -c '^ *{' "$o"
+es-sample -h "$h" -c - -o "$o" "$1" && zgrep -c '^ *{' "$o" || exit 1
+
+[ -n "$d" ] && curl -sSXDELETE $h/$1 && echo
+
+exit 0
