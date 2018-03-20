32ae3a7be4d49068d58822107ca3988154607e27
Jens Wille <jens.wille@gmail.com>
2018-03-20 16:53:34 +0100

    json_pp.rb: Added option for JSON canonicalization.

--- json_pp.rb
+++ json_pp.rb
@@ -4,6 +4,10 @@ require 'json'
 
 module JSON
 
+  extend self
+
+  PP_OPT = :_json_pp_canonical
+
   class PP_KEY < String
 
     def eql?(*)
@@ -20,20 +24,41 @@ module JSON
 
   end
 
-  def self.pp(source, opt = {})
-    pretty_generate(parse(source, opt.merge(object_class: PP_OBJECT)))
+  def pp(source, opt = {})
+    obj = parse(source, opt.merge(object_class: PP_OBJECT))
+    pretty_generate(opt[PP_OPT] ? send(PP_OPT, obj) : obj)
   rescue ParserError => err
     warn "#{err.class}: #{err}"
     source
+  end
+
+  def pc(source, opt = {})
+    pp(source, opt.merge(PP_OPT => true))
+  end
+
+  private
+
+  def _json_pp_canonical(obj)
+    case obj
+      when Hash
+        obj.class.new.tap { |res|
+          obj.keys.sort.each { |k| res[k] = send(PP_OPT, obj[k]) } }
+      when Array
+        obj.map { |v| send(PP_OPT, v) }.sort_by(&:to_s)
+      else
+        obj
+    end
   end
 
 end
 
 if $0 == __FILE__
   if ARGV.include?('-h') || ARGV.include?('--help')
-    abort "Usage: #{$0} [-h|--help] [-l|--lines] [SOURCE...]"
+    abort "Usage: #{$0} [-h|--help] [-l|--lines] [-c|--canon] [SOURCE...]"
   end
 
+  meth = ARGV.delete('--canon') || ARGV.delete('-c') ? :pc : :pp
   args = ARGV.delete('--lines') || ARGV.delete('-l') ? ARGF : [ARGF.read]
-  args.each { |source| puts JSON.pp(source) }
+
+  args.each { |source| puts JSON.send(meth, source) }
 end
