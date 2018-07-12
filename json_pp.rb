af0dbaa3ff1dfee7d232c7df46f40dba5bf66284
Jens Wille <jens.wille@gmail.com>
2018-07-12 15:51:54 +0200

    json_pp.rb: Use nuggets v1.6.0.

--- json_pp.rb
+++ json_pp.rb
@@ -1,56 +1,7 @@
 #! /usr/bin/env ruby
 
-require 'json'
-
-module JSON
-
-  extend self
-
-  PP_OPT = :_json_pp_canonical
-
-  class PP_KEY < String
-
-    def eql?(*)
-      false
-    end
-
-  end
-
-  class PP_OBJECT < Hash
-
-    def []=(k, v)
-      super(PP_KEY.new(k), v)
-    end
-
-  end
-
-  def pp(source, opt = {})
-    obj = parse(source, opt.merge(object_class: PP_OBJECT))
-    pretty_generate(opt[PP_OPT] ? send(PP_OPT, obj) : obj)
-  rescue ParserError => err
-    warn "#{err.class}: #{err}"
-    source
-  end
-
-  def pc(source, opt = {})
-    pp(source, opt.merge(PP_OPT => true))
-  end
-
-  private
-
-  def _json_pp_canonical(obj)
-    case obj
-      when Hash
-        obj.class.new.tap { |res|
-          obj.keys.sort.each { |k| res[k] = send(PP_OPT, obj[k]) } }
-      when Array
-        obj.map { |v| send(PP_OPT, v) }.sort_by(&:to_s)
-      else
-        obj
-    end
-  end
-
-end
+require 'nuggets/json/multi'
+require 'nuggets/json/canonical'
 
 if $0 == __FILE__
   if ARGV.include?('-h') || ARGV.include?('--help')
@@ -60,5 +11,10 @@ if $0 == __FILE__
   meth = ARGV.delete('--canon') || ARGV.delete('-c') ? :pc : :pp
   args = ARGV.delete('--lines') || ARGV.delete('-l') ? ARGF : [ARGF.read]
 
-  args.each { |source| puts JSON.send(meth, source) }
+  args.each { |source| puts begin
+    JSON.send(meth, source)
+  rescue ParserError => err
+    warn "#{err.class}: #{err}"
+    source
+  end }
 end
