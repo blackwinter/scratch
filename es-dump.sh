3628de6f131d6da05b966643256f2f1e8808f0f7
Jens Wille <jens.wille@gmail.com>
2019-03-27 09:53:15 +0100

    es-dump.sh: Optionally check if output file already exists.

--- es-dump.sh
+++ es-dump.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
-[ "$1" == "-d" ] && d=1 && shift
-[ $# -lt 2 -o $# -gt 3 ] && echo "Usage: $0 [-d] <index> <name> [<host>]" && exit 1
+[ "$1" == "-c" ] && c=$1 && shift
+[ "$1" == "-d" ] && d=$1 && shift
+
+[ $# -lt 2 -o $# -gt 3 ] && echo "Usage: $0 [-c|-d] <index> <name> [<host>]" && exit 1
 
 h="${3:-http://localhost:9200}"
 o="$1"; [ "${o:$((${#o}-1))}" == "-" ] || o+="-"; o+="$2.jsonl.gz"
 
-es-sample -h "$h" -c - -o "$o" "$1" && zgrep -c '^ *{' "$o" || exit 1
-
-[ -n "$d" ] && curl -sSXDELETE $h/$1 && echo
-
-exit 0
+if [ -z "$c" ]; then
+  es-sample -h "$h" -c - $d -o "$o" "$1" && zgrep -c '^ *{' "$o" || exit 1
+  exit 0
+else
+  [ -f "$o" ]
+fi
