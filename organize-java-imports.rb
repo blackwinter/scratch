8ebf58eb4dba231ca9541131effea64e64d12d25
Jens Wille <jens.wille@hbz-nrw.de>
2023-10-02 17:37:55 +0200

    organize-java-imports.rb: Fix.

--- organize-java-imports.rb
+++ organize-java-imports.rb
@@ -1,9 +1,10 @@
 #! /usr/bin/env ruby
 
-out, imp, buf, del, pkg, com = [], Hash.new { |h, k| h[k] = [] }, [], [], '', nil
+out, imp, buf, del, pkg, com, idx =
+  [], Hash.new { |h, k| h[k] = [] }, [], [], nil, nil, -1
 
 ARGF.each { |line|
-  out << line.dup
+  out[idx += 1] = line.dup
 
   line.sub!(/(?:\A|.*\s)\*\//, '') ? com = nil  : com ? next :
   line.sub!(/\/\*(?:\z|\s.*)/, '') ? com = true : nil
@@ -15,9 +16,9 @@ ARGF.each { |line|
 
   case line
     when /\Apackage\s+([\w.]+);/
-      pkg = $1
+      pkg = $1 + '.'
     when /\Aimport\s+(.*?)([^.\s*]+)\s*;/
-      ($1 == pkg + '.' ? del : imp[$2]) << $. - 1
+      ($1 == pkg ? del : imp[$2]) << idx
     else
       line.scan(/\b(#{Regexp.union(*imp.keys)})\b/) {
         del.concat(imp.delete($1).drop(1)) if imp.key?($1)
@@ -33,11 +34,11 @@ out.each_with_index { |line, num|
   if line =~ /\A(?:\/\/)?import\s+/
     buf << line
   else
-    puts buf.sort_by { |i| i.sub(/\A\/\//, '') }, line
+    puts buf.sort_by { |i| i.sub(/\A\/\//, '').sub(/;\Z/, '') }, line
     buf.clear
   end
 }
 
 puts buf
 
-abort 'No package declaration!' if pkg.empty?
+abort 'No package declaration!' unless pkg
